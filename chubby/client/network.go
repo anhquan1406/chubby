afccb0cffecbd8e025bf2c985a5e506554702213
angqun <anhquanyl1404@gmail.com>
2026-05-30 00:49:47 +0700

    Chore: Cập nhật cấu hình IP (127.0.0.1) để chạy mượt mà trên môi trường Windows Local

--- chubby/client/network.go
+++ chubby/client/network.go
@@ -4,10 +4,8 @@ package client
 
 // We assume that any Chubby node must have one of these addresses.
 // Yes this is gross but we're doing it anyway because of time constraints
-var PossibleServerAddrs = map[string]bool {
-	"172.20.128.1:5379": true,
-	"172.20.128.2:5379": true,
-	"172.20.128.3:5379": true,
-	"172.20.128.4:5379": true,
-	"172.20.128.5:5379": true,
+var PossibleServerAddrs = map[string]bool{
+	"127.0.0.1:5379": true,
+	"127.0.0.1:6379": true,
+	"127.0.0.1:7379": true,
 }
